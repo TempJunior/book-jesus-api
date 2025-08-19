c085b7e9d18d5dbeaf279fc12acbeba9e0b972e8
TempJunior <junior_sadaanzinhoa@hotmail.com>
2025-08-19 17:36:54 -0300

    Config, anotações de classe teste

--- src/test/java/com/tempjunior/book_jesus_application/BookJesusApplicationTests.java
+++ src/test/java/com/tempjunior/book_jesus_application/BookJesusApplicationTests.java
@@ -1,13 +1,18 @@
 package com.tempjunior.book_jesus_application;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BookJesusApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
